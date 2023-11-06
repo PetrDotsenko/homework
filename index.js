28f04ece3b54601e604fd34c71e59c4ae07fb007
PetrDotsenko <88041442+PetrDotsenko@users.noreply.github.com>
2023-11-06 19:07:24 +1000

    initialCommit

---
+++ index.js
@@ -0,0 +1,9 @@
+let toClickOnStar = function () {
+    const stars = Array.from(document.querySelectorAll('.star'));
+    for (let i = 0; i < stars.length; i++) {
+    
+    }   
+}
+
+const star = document.querySelector('.star');
+star.addEventListener("click", toClickOnStar);
